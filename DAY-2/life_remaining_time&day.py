ba651876bbc818a833bbaf061c770e4ac8630e84
sumitmy <sumitmy2018@gmail.com>
2023-05-09 10:48:37 +0530

    DAY-2 work completed

---
+++ DAY-2/life_remaining_time&day.py
@@ -0,0 +1,29 @@
+current_age=int(input("what is your current age: "))
+max_age= int(input("enter the number of max age: "))
+age_remaining=max_age-current_age
+print("remaining age: ",age_remaining)
+month=age_remaining*12
+print("month: ",month)
+week=age_remaining*52
+print("week: ",week)
+day=age_remaining*365
+print("day: ",day)
+hour=day*24
+print("hour: ",hour)
+mint=hour*60
+print("mint: ",mint)
+seconds=mint*60
+print("seconds: ",seconds)
+milisecond=mint*60000
+print("milisecond: ",milisecond)
+
+
+
+
+
+
+
+
+
+
+
